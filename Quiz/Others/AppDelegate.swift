2a326da7e59cdeabf52ce058d0e06001200fb0b9
GuillaumeBourlart <bourlartguillaume@gmail.com>
2023-08-05 12:35:27 +0300

    test

--- Quiz/Others/AppDelegate.swift
+++ Quiz/Others/AppDelegate.swift
@@ -28,8 +28,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         }
     }
     
-    
-    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
         // Initialize Firebase
